ffb8fdbe8d3bd9266ef986e4fb880d0a7453a5ce
NovandiKevin <kevinpret27@gmail.com>
2021-08-17 23:34:57 +0700

    header  cookies

--- App/App.js
+++ App/App.js
@@ -26,9 +26,9 @@ app.use(
   })
 )
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Headers", "*")
-  // res.header("Access-Control-Allow-Credentials", true)
-  // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+  // res.header("Access-Control-Allow-Headers", "*")
+  res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
   next()
 })
 app.use(morgan("tiny"))

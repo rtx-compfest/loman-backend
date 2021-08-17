354f647fc940569b6f290bdaa3795f9b5541bac6
NovandiKevin <kevinpret27@gmail.com>
2021-08-18 05:14:08 +0700

    cookies perbaikan

--- Controller/UsersController.js
+++ Controller/UsersController.js
@@ -68,7 +68,7 @@ router.post("/login", async function (req, res, next) {
   res
     .cookie("token", data.token, {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60000),
-      httpOnly: true,
+      httpOnly: false,
       signed: true,
       sameSite: "none",
       secure: true,

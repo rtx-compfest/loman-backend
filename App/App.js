70bacbe16c70a8e8ceda5c3e1bc378b49bae553e
NovandiKevin <kevinpret27@gmail.com>
2021-08-18 07:20:57 +0700

    Header

--- App/App.js
+++ App/App.js
@@ -35,15 +35,9 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "*")
   // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  )
+  res.setHeader("Access-Control-Allow-Methods", "*")
   // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,content-type,set-cookie"
-  )
+  res.setHeader("Access-Control-Allow-Headers", "*")
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true)

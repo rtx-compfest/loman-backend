d67a772b37f026231bec5c96c967f73155119690
NovandiKevin <kevinpret27@gmail.com>
2021-08-18 07:22:32 +0700

    Back Config

--- App/App.js
+++ App/App.js
@@ -27,24 +27,7 @@ app.use(cmpression())
 //   })
 // )
 
-// app.use(
-//   Cors({ origin: "*", credentials: true, exposedHeaders: ["set-cookie"] })
-// )
-
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  // Request methods you wish to allow
-  res.setHeader("Access-Control-Allow-Methods", "*")
-  // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "*")
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true)
-
-  // Pass to next layer of middleware
-  next()
-})
+app.use(Cors({ origin: true, credentials: true }))
 
 app.set("trust proxy", 1)
 app.use(

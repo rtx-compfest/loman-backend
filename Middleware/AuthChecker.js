55527fc41f88f1e9aaca136e90dc167d79d44967
NovandiKevin <kevinpret27@gmail.com>
2021-08-22 00:33:19 +0700

    Remove unused

--- Middleware/AuthChecker.js
+++ Middleware/AuthChecker.js
@@ -12,18 +12,6 @@ const AuthChecker = function (req, res, next) {
   } else {
     verify(req, res, next)
   }
-  // if (url.length > 1) {
-  //   if (
-  //     (url[1] == "image") |
-  //     ((url[1] === "user") & ((url[2] === "login") | (url[2] === "register")))
-  //   ) {
-  //     next()
-  //   } else {
-  //     verify(req, res, next)
-  //   }
-  // } else {
-  //   verify(req, res, next)
-  // }
 }
 
 module.exports = AuthChecker

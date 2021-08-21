cec692597f5128bf6d143276a54640190a92a832
NovandiKevin <kevinpret27@gmail.com>
2021-08-21 23:41:04 +0700

    Route accepted

--- Middleware/AuthChecker.js
+++ Middleware/AuthChecker.js
@@ -1,18 +1,29 @@
 const { verify } = require("../Util/JWT")
+const allowedURL = [
+  "POST /user/login",
+  "POST /user/register",
+  "GET /image",
+  "GET /donation_program",
+]
 const AuthChecker = function (req, res, next) {
-  let url = req.path.split("/")
-  if (url.length > 1) {
-    if (
-      (url[1] == "image") |
-      ((url[1] === "user") & ((url[2] === "login") | (url[2] === "register")))
-    ) {
-      next()
-    } else {
-      verify(req, res, next)
-    }
+  let url = `${req.method} ${req.path}`
+  if (new RegExp(allowedURL.join("|")).test(url)) {
+    next()
   } else {
     verify(req, res, next)
   }
+  // if (url.length > 1) {
+  //   if (
+  //     (url[1] == "image") |
+  //     ((url[1] === "user") & ((url[2] === "login") | (url[2] === "register")))
+  //   ) {
+  //     next()
+  //   } else {
+  //     verify(req, res, next)
+  //   }
+  // } else {
+  //   verify(req, res, next)
+  // }
 }
 
 module.exports = AuthChecker

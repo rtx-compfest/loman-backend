a4c3414f95d79120383161aefebe9174200b721d
NovandiKevin <kevinpret27@gmail.com>
2021-08-22 09:11:42 +0700

    Change staging

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 const app = require("../App/App")
 require("dotenv").config()
 const port = process.env.PORT || 1000
-
+//Listen
 app.listen(port, async () => {
   console.log(`REST at http://localhost:${port}`)
 })
